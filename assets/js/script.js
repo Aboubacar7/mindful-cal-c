7a79df40fa4b40909f4564b7d50bbb2bf6311a99
Meljska-Fawn <meli.explores@gmail.com>
2023-02-15 15:04:53 -0800

    added js for calcCaloriesBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=a`)
     .then(function (data) {
         console.log(data);
         results = (data.meals);
-        console.log(results);       
+        console.log(results);
     });
 
 fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=b`)
@@ -55,19 +55,19 @@ fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=b`)
 
 
 function showMeals(results) {
-    
-let configuredData = [];
 
-mealDropdown.innerHTML = "";
+    let configuredData = [];
+
+    mealDropdown.innerHTML = "";
     for (let i = 0; i < results.length; i++) {
 
         mealDropdown.innerHTML += `<a>${results[i].strMeal}</a>`;
-        
-         let obj = {meal:results[i].strMeal}
-        for(let y = 0; y < 20; y++){
-            if(results[i][`strMeasure${y}`] && results[i][`strIngredient${y}`]){
-            let obj = {measure: results[i][`strMeasure${y}`], ingredient: results[i][`strIngredient${y}`]}
-            configuredData.push(obj)
+
+        let obj = { meal: results[i].strMeal }
+        for (let y = 0; y < 20; y++) {
+            if (results[i][`strMeasure${y}`] && results[i][`strIngredient${y}`]) {
+                let obj = { measure: results[i][`strMeasure${y}`], ingredient: results[i][`strIngredient${y}`] }
+                configuredData.push(obj)
             }
         }
         obj["data"] = configuredData;
@@ -78,64 +78,64 @@ mealDropdown.innerHTML = "";
 }
 
 
-calcCaloriesBtn.addEventListener("click", function() {
+mealDropdown.addEventListener("click", function (event) {
+    console.log(event.target.innerHTML);
 
-    let measure = currentMealDetails.data[i].measure;
-    measure = measure.replace(" ","%20");
-
-    let ingredient1 = currentMealDetails.data[i].ingredient;
-    ingredient1 = ingredient1.replace(" ","%20");
-
-    let ingredient = measure+"%2C"+ingredient1;
-    console.log(ingredient);
-
-    var requestUrl2 = `https://api.edamam.com/api/nutrition-data?app_id=e9200f1f&app_key=%2044c8f29589b07d2306fc29112f74e5d2&nutrition-type=cooking&ingr=${ingredient}`;
-    fetch(requestUrl2)
-    .then(resp => resp.json())
-    .then(data => showTable(data))
-})
-
-function showTable(data) {
-console.log(data);
-
-tableBody.innerHTML="";
-tableBody.innerHTML+=`
-<tr>
-<td>${data.totalWeight}</td>
-<td>${data.totalNutrients.CHOCDF.unit}</td>
-<td>${data.ingredients[0].text}</td>
-<td>${data.calories}</td>
-</tr>
-`
-}
-
-mealDropdown.addEventListener("click", function(event) {
-console.log(event.target.innerHTML);
-
-var currentMeal = event.target.innerText;
-var currentMealDetails = "";
+    var currentMeal = event.target.innerText;
+    currentMealDetails = "";
 
     recipeTitleEl.textContent = "";
-    let recipeName = document.createElement('h5'); 
+    let recipeName = document.createElement('h5');
     recipeName.textContent = currentMeal;
     recipeTitleEl.append(recipeName);
 
-for (let i = 0; i < totalArray.length; i++) {
+    for (let i = 0; i < totalArray.length; i++) {
 
-    if (currentMeal.trim() === totalArray[i].meal.trim()) {
-    
-    currentMealDetails = totalArray[i];
-}
-}
+        if (currentMeal.trim() === totalArray[i].meal.trim()) {
 
-for (let i = 0; i<currentMealDetails.data.length; i++) {
-    document.querySelector("table").innerHTML +=`
+            currentMealDetails = totalArray[i];
+        }
+    }
+
+    for (let i = 0; i < currentMealDetails.data.length; i++) {
+        document.querySelector("table").innerHTML += `
     <tr>
     <td>${currentMealDetails.data[i].measure}</td>
     <td>${currentMealDetails.data[i].ingredient}</td>
     </tr>
     `
-}
+    }
 
-console.log(currentMealDetails)
+    console.log(currentMealDetails)
 })
+
+calcCaloriesBtn.addEventListener("click", function () {
+
+    const ingredientURLs = [];
+
+    for (let i = 0; i < currentMealDetails.data.length; i++) {
+
+        const encodedMeasure = encodeURIComponent(currentMealDetails.data[i].measure);
+
+        const encodedIngredient = encodeURIComponent(currentMealDetails.data[i].ingredient);
+
+        ingredientURLs.push(fetch(`https://api.edamam.com/api/nutrition-data?app_id=e9200f1f&app_key=%2044c8f29589b07d2306fc29112f74e5d2&nutrition-type=cooking&ingr=${encodedMeasure}%20${encodedIngredient}`).then(response => response.json()));
+    }
+    Promise.all(ingredientURLs)
+        .then(data => showTable(data))
+})
+
+function showTable(data) {
+    console.log(data);
+
+    for (i = 0; i < data.length; i++) {
+        tableBody.innerHTML += `
+    <tr>
+    <td>${data[i].totalWeight}</td>
+    <td>${data[i].totalNutrients?.CHOCDF?.unit}</td>
+    <td>${data[i].ingredients?.length > 0 ? data[i].ingredients[0].text.slice(data[i].ingredients[0].text.indexOf(" ")) : "no listed ingredients"}</td>
+    <td>${data[i].calories}</td>
+    </tr>
+    `
+    }
+}
